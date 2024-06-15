03763fa312beef46984bb86fe22a85588b3453cf
Kateryna D <july_info@ukr.net>
2024-06-15 18:01:08 +0300

    commit

--- js/task-4.js
+++ js/task-4.js
@@ -1,49 +1,28 @@
 const getTotalBalanceByGender = (users, gender) => {
-  return users
-    .filter(user => user.gender === gender)
-    .reduce((totalBalance, user) => totalBalance + user.balance, 0);
+  let totalBalance = 0;
+  for (let i = 0; i < users.length; i++) {
+    if (users[i].gender === gender) {
+      totalBalance += users[i].balance;
+    }
+  }
+  return totalBalance;
 };
 
+// Перевірка роботи функції
 const clients = [
-  {
-    name: "Moore Hensley",
-    gender: "male",
-    balance: 2811
-  },
-  {
-    name: "Sharlene Bush",
-    gender: "female",
-    balance: 3821
-  },
-  {
-    name: "Ross Vazquez",
-    gender: "male",
-    balance: 3793
-  },
-  {
-    name: "Elma Head",
-    gender: "female",
-    balance: 2278
-  },
-  {
-    name: "Carey Barr",
-    gender: "male",
-    balance: 3951
-  },
-  {
-    name: "Blackburn Dotson",
-    gender: "male",
-    balance: 1498
-  },
-  {
-    name: "Sheree Anthony",
-    gender: "female",
-    balance: 2764
-  }
+  { name: "Moore Hensley", gender: "male", balance: 2811 },
+  { name: "Sharlene Bush", gender: "female", balance: 3821 },
+  { name: "Ross Vazquez", gender: "male", balance: 3793 },
+  { name: "Elma Head", gender: "female", balance: 2278 },
+  { name: "Carey Barr", gender: "male", balance: 3951 },
+  { name: "Blackburn Dotson", gender: "male", balance: 1498 },
+  { name: "Sheree Anthony", gender: "female", balance: 2764 }
 ];
 console.log('-----------------------------------------task-4------------------------------------------------------');
 console.log(getTotalBalanceByGender(clients, "male")); // 12053
 console.log(getTotalBalanceByGender(clients, "female")); // 8863
+
+
 
 
 
